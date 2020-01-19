dec99e20c79aef3af6db74d3af5fb642f107a78a
Ravi Tripathi <ravitripathi1996@gmail.com>
2020-01-19 18:07:15 +0530

    Added Project

---
+++ Beaver/Beaver/Beaver.h
@@ -0,0 +1,19 @@
+//
+//  Beaver.h
+//  Beaver
+//
+//  Created by Ravi Tripathi on 19/01/20.
+//  Copyright © 2020 Ravi Tripathi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Beaver.
+FOUNDATION_EXPORT double BeaverVersionNumber;
+
+//! Project version string for Beaver.
+FOUNDATION_EXPORT const unsigned char BeaverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Beaver/PublicHeader.h>
+
+
