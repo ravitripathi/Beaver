52bb46729e4f919ab7d2c32c51ea9d718f02c185
Ravi Tripathi <ravitripathi1996@gmail.com>
2020-01-19 18:10:27 +0530

    Added Example Project

---
+++ Example/BeaverExample/BeaverExample/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BeaverExample
+//
+//  Created by Ravi Tripathi on 19/01/20.
+//  Copyright © 2020 Ravi Tripathi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Example/BeaverExample/BeaverExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaverExample
+//
+//  Created by Ravi Tripathi on 19/01/20.
+//  Copyright © 2020 Ravi Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
